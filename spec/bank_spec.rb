f7c6f2dcff0a0cd2c925c37560c6e3c14fc446b1
Maggie Hibberd <maggiehibberd@gmail.com>
2021-05-02 19:42:14 +0100

    All statement tests previousl in the bank_spec moved to statement_spec and passing.

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -49,33 +49,3 @@ describe '#withdrawl' do
   end 
 end
 
-
-#   it 'prints a formated string with a statement title, deposit(1000) and date 10/01/2012' do
-#     bank = Bank.new
-#     bank.deposit(1000, '10/01/2012')
-#   expect(bank.statement).to include 'date || credit || debit || balance'
-#   expect(bank.statement).to include '10/01/2012 || 1000 || || 1000'
-#   end
-#   it 'prints a formated string with a statement title, deposit(1000), date 10/01/2012 & deposit(2000), date 13/01/2012 ' do
-#     bank = Bank.new
-#     bank.deposit(1000, '10/01/2012')
-#     bank.deposit(2000, '13/01/2012')
-#   expect(bank.statement).to include 'date || credit || debit || balance'
-#   expect(bank.statement).to include '13/01/2012 || 2000 || || 3000'
-#   expect(bank.statement).to include '10/01/2012 || 1000 || || 1000'
-  
-#   end
-#   it 'prints a formated string with a statement title, multiple deposits and a withdrawl' do
-#     bank = Bank.new
-#     bank.deposit(1000, '10/01/2012')
-#     bank.deposit(2000, '13/01/2012')
-#     bank.withdrawl(500,'14/01/2012')
-#   expect(bank.statement).to include 'date || credit || debit || balance'
-#   expect(bank.statement).to include '14/01/2012 || || 500 || 2500'
-#   expect(bank.statement).to include '13/01/2012 || 2000 || || 3000'
-#   expect(bank.statement).to include '10/01/2012 || 1000 || || 1000'
-  
- 
-#   end
-#   end
-# end 
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -15,5 +15,25 @@ describe '#statement' do
   expect(statement.print_statement).to include 'date || credit || debit || balance'
   expect(statement.print_statement).to include '10/01/2012 || 1000 || || 1000'
   end
+  it 'prints a formated string with a statement title, deposit(1000) and date 10/01/2012' do
+    bank = Bank.new
+    bank.deposit(1000, '10/01/2012')
+    bank.deposit(2000, '13/01/2012')
+    statement = Statement.new(bank)
+  expect(statement.print_statement).to include 'date || credit || debit || balance'
+  expect(statement.print_statement).to include '10/01/2012 || 1000 || || 1000'
+  expect(statement.print_statement).to include '10/01/2012 || 1000 || || 1000'
+  end
+    it 'prints a formated string with a statement title, multiple deposits and a withdrawl' do
+    bank = Bank.new
+    bank.deposit(1000, '10/01/2012')
+    bank.deposit(2000, '13/01/2012')
+    bank.withdrawl(500,'14/01/2012')
+    statement = Statement.new(bank)
+  expect(statement.print_statement).to include 'date || credit || debit || balance'
+  expect(statement.print_statement).to include '14/01/2012 || || 500 || 2500'
+  expect(statement.print_statement).to include '13/01/2012 || 2000 || || 3000'
+  expect(statement.print_statement).to include '10/01/2012 || 1000 || || 1000'
   end 
+end
 end

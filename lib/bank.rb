dd6b85b09ccd0af112979a8ca3779e4fd17d0d7e
Maggie Hibberd <maggiehibberd@gmail.com>
2021-05-27 17:15:03 +0100

    date parameter updated to go stright into formated string rather than to string via a variable

--- lib/bank.rb
+++ lib/bank.rb
@@ -11,18 +11,15 @@ class Bank
   TODAYS_DATE = Time.now.strftime('%d/%m/%Y')
 
   def deposit(money_in, date = TODAYS_DATE)
-    
-    @date = date
     @balance += money_in
-    @transactions << "#{@date} || #{'%.2f' % money_in} || #{@money_out}|| #{format('%.2f', @balance)}\n"
+    @transactions << "#{date} || #{'%.2f' % money_in} || #{@money_out}|| #{format('%.2f', @balance)}\n"
   end
 
   def withdrawal(money_out, date = TODAYS_DATE)
     raise 'Insufficient funds in your account' if @balance.zero?
 
-    @date = date
     @money_out = money_out
     @balance -= money_out
-    @transactions << "#{@date} || || #{format('%.2f', @money_out)} || #{format('%.2f', @balance)}\n"
+    @transactions << "#{date} || || #{format('%.2f', @money_out)} || #{format('%.2f', @balance)}\n"
   end
 end
